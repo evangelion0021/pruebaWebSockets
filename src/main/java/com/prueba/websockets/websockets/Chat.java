1f8bcd381b56115ea8b799a23d7c8ba3b5fc6c89
Gustavo <gustavotarazona@gmail.com>
2018-02-23 10:47:04 -0500

    se creo un segundo websocket para reforzar algunos conceptos

---
+++ src/main/java/com/prueba/websockets/websockets/Chat.java
@@ -0,0 +1,108 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.prueba.websockets.websockets;
+import com.prueba.websockets.model.Message;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArraySet;
+import javax.websocket.OnClose;
+import javax.websocket.OnError;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+import java.util.Set;
+import javax.websocket.EncodeException;
+import javax.websocket.server.PathParam;
+/**
+ *
+ * @author PC
+ */
+@ServerEndpoint(value = "/chat2/{name}", decoders = MessageDecoder.class, encoders = MessageEncoder.class)
+public class Chat {
+    private Session sesion;
+    private static Set<Chat> chatEndPoints= new CopyOnWriteArraySet<>();
+    private static HashMap<String, String> users=new HashMap<>();
+    
+    @OnOpen
+    public void onOpen(Session sesion,
+            @PathParam("name") String username)throws IOException, EncodeException{
+        this.sesion=sesion;
+        chatEndPoints.add(this);
+        users.put(sesion.getId(), username);
+        Message mensaje=new Message();
+        mensaje.setFrom(username);
+        mensaje.setContent("Connected!");
+        broadcast(mensaje);
+        
+        
+    }
+    
+    @OnMessage
+    public void onMessage(Session sesion, Message mensaje)throws IOException,EncodeException{
+        mensaje.setFrom(users.get(sesion.getId()));
+        String men[]=mensaje.getContent().split("-");
+        if(men.length>1){
+            mensaje.setContent(men[1]);
+            mensaje.setTo(men[0]);
+        }
+        if(mensaje.getTo()!=null){
+            broadcast2(mensaje);
+        }else{
+            broadcast(mensaje);
+        }
+        
+    }
+    
+    @OnClose
+    public void onClose(Session sesion)throws IOException,EncodeException{
+        chatEndPoints.remove(this);
+        Message mensaje=new Message();
+        mensaje.setFrom(users.get(sesion.getId()));
+        mensaje.setContent("Disconnected!");
+        broadcast(mensaje);
+        
+    }
+    
+    @OnError
+    public void onError(Session sesion, Throwable throwable){
+        
+    }
+    
+    private static void broadcast(Message mensaje)throws IOException, EncodeException{
+        chatEndPoints.forEach(endPoint -> {
+            synchronized(endPoint){
+                try{
+                    endPoint.sesion.getBasicRemote().sendObject(mensaje);
+                }catch(IOException | EncodeException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+    
+    private static void broadcast2(Message mensaje)throws IOException, EncodeException{
+        for(Map.Entry<String,String> entry:users.entrySet()){
+            if(entry.getValue().equals(mensaje.getTo())){
+                chatEndPoints.forEach(endPoint -> {
+            synchronized(endPoint){
+                try{
+                    if(endPoint.sesion.getId().equals(entry.getKey())){
+                        endPoint.sesion.getBasicRemote().sendObject(mensaje);
+                    }
+                    
+                }catch(IOException | EncodeException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+            }
+        }
+        
+        
+    }
+}
--- src/main/webapp/websockets.js
+++ src/main/webapp/websockets.js
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 var ws;
+var ws2;
 
 function connect() {
     var username = document.getElementById("username").value;
@@ -23,10 +24,41 @@ function connect() {
 
 function send() {
     var content = document.getElementById("msg").value;
+    
     var json = JSON.stringify({
         "content":content
     });
+    
+    
 
     ws.send(json);
+}
+
+function connect2() {
+    var username = document.getElementById("username2").value;
+    
+    var host = document.location.host;
+    var pathname = document.location.pathname;
+    
+    ws2 = new WebSocket("ws://" +host  + pathname + "chat2/" + username);
+
+    ws2.onmessage = function(event) {
+    var log = document.getElementById("log2");
+        console.log(event.data);
+        var message = JSON.parse(event.data);
+        log.innerHTML += message.from + " : " + message.content + "\n";
+    };
+}
+
+function send2() {
+    var content = document.getElementById("msg2").value;
+    
+    var json = JSON.stringify({
+        "content":content
+    });
+    
+    
+
+    ws2.send(json);
 }
 
